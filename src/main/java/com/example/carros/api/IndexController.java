42c8035e43fa994a8f4651d4c371f8613b6a7f70
mateus_techno <mateus@technosoftware.com.br>
2020-02-24 16:55:50 -0300

    First Commit

---
+++ src/main/java/com/example/carros/api/IndexController.java
@@ -0,0 +1,29 @@
+package com.example.carros.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+	
+	@GetMapping
+	public String get() {
+		return "Get Spring Boot";
+	}
+	
+	@PostMapping("/login")
+	public String login(@RequestParam("login") String login, @RequestParam("senha") String senha) {
+		return "Login: " + login + ", Senha: " + senha;
+	}
+	
+	@PostMapping("/login/{login}/{senha}")
+	public String getParam(@PathVariable("login") String login, @PathVariable("senha") String senha) {
+		return "Login: " + login + ", Senha: " + senha;
+	}
+	
+}
